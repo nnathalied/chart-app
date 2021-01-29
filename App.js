2218309523787437b21785d5548c98df0db1f0ac
nndea <nndea@users.noreply.github.com>
2021-01-29 11:08:28 -0800

    first chart added!

--- App.js
+++ App.js
@@ -2,26 +2,47 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, SafeAreaView} from 'react-native';
 
+import { Chart, Line, Area, HorizontalAxis, VerticalAxis } from 'react-native-responsive-linechart'
+
+
 const logo = {
   uri: 'https://reactnative.dev/img/tiny_logo.png',
   width: 64,
   height: 64
 };
 
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart
-} from "react-native-chart-kit";
-
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
+
+      <Chart
+        style={{ height: 200, width: 400 }}
+        data={[
+          { x: -2, y: 15 },
+          { x: -1, y: 10 },
+          { x: 0, y: 12 },
+          { x: 1, y: 7 },
+          { x: 2, y: 6 },
+          { x: 3, y: 8 },
+          { x: 4, y: 10 },
+          { x: 5, y: 8 },
+          { x: 6, y: 12 },
+          { x: 7, y: 14 },
+          { x: 8, y: 12 },
+          { x: 9, y: 13.5 },
+          { x: 10, y: 18 },
+        ]}
+        padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
+        xDomain={{ min: -2, max: 10 }}
+        yDomain={{ min: 0, max: 20 }}
+      >
+        <VerticalAxis tickCount={11} theme={{ labels: { formatter: (v) => v.toFixed(2) } }} />
+        <HorizontalAxis tickCount={5} />
+        <Area theme={{ gradient: { from: { color: '#ffa502' }, to: { color: '#ffa502', opacity: 0.4 } }}} />
+        <Line theme={{ stroke: { color: '#ffa502', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
+      </Chart>
           <Text style={styles.headerText}> Charts Demo </Text>
           <Text> I want to put a chart here </Text>
           <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
